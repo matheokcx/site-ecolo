118c95d4dae0be6e7bd78ae79a95498774a3deb7
Mathéo Deleplanque <matheo.deleplanque@gmail.com>
2026-02-12 00:07:02 +0100

    modifs

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,7 +1,10 @@
 const ContactsPage = () => {
     return (
-      <div>
-
+      <div style={{ width: '100%', height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <div>
+            mail: matheo.deleplanque@gmail.com
+            tel: +33707070707
+        </div>
       </div>
     );
 };
